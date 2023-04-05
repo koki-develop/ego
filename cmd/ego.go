8cef0866812eff750e730a04242984444ead55c2
koki-develop <kou.pg.0131@gmail.com>
2023-04-05 17:02:12 +0900

    add --fg and --bg flag

--- cmd/ego.go
+++ cmd/ego.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"io"
 	"strconv"
 )
@@ -8,9 +9,16 @@ import (
 type egoOptions struct {
 	NoNewline     bool
 	EnableEscapes bool
+
+	Foreground string
+	Background string
 }
 
 func ego(w io.Writer, args []string, options egoOptions) error {
+	if err := style(w, options); err != nil {
+		return err
+	}
+
 	for i, arg := range args {
 		if options.EnableEscapes {
 			if err := interpretEscapes(w, arg); err != nil {
@@ -28,6 +36,11 @@ func ego(w io.Writer, args []string, options egoOptions) error {
 			}
 		}
 	}
+
+	if err := resetStyle(w); err != nil {
+		return err
+	}
+
 	if !options.NoNewline {
 		if _, err := w.Write([]byte{'\n'}); err != nil {
 			return err
@@ -113,6 +126,87 @@ func interpretEscapes(w io.Writer, s string) error {
 				return err
 			}
 		}
+	}
+
+	return nil
+}
+
+var (
+	ansiFg = map[string][]byte{
+		"black":     []byte("[30m"),
+		"red":       []byte("[31m"),
+		"green":     []byte("[32m"),
+		"yellow":    []byte("[33m"),
+		"blue":      []byte("[34m"),
+		"magenta":   []byte("[35m"),
+		"cyan":      []byte("[36m"),
+		"white":     []byte("[37m"),
+		"hiblack":   []byte("[90m"),
+		"hired":     []byte("[91m"),
+		"higreen":   []byte("[92m"),
+		"hiyellow":  []byte("[93m"),
+		"hiblue":    []byte("[94m"),
+		"himagenta": []byte("[95m"),
+		"hicyan":    []byte("[96m"),
+		"hiwhite":   []byte("[97m"),
+	}
+
+	ansiBg = map[string][]byte{
+		"black":     []byte("[40m"),
+		"red":       []byte("[41m"),
+		"green":     []byte("[42m"),
+		"yellow":    []byte("[43m"),
+		"blue":      []byte("[44m"),
+		"magenta":   []byte("[45m"),
+		"cyan":      []byte("[46m"),
+		"white":     []byte("[47m"),
+		"hiblack":   []byte("[100m"),
+		"hired":     []byte("[101m"),
+		"higreen":   []byte("[102m"),
+		"hiyellow":  []byte("[103m"),
+		"hiblue":    []byte("[104m"),
+		"himagenta": []byte("[105m"),
+		"hicyan":    []byte("[106m"),
+		"hiwhite":   []byte("[107m"),
+	}
+)
+
+func style(w io.Writer, options egoOptions) error {
+	if options.Foreground != "" {
+		ansi, ok := ansiFg[options.Foreground]
+		if !ok {
+			return fmt.Errorf("unsupported foreground color: %s", options.Foreground)
+		}
+		if _, err := w.Write([]byte{'\x1b'}); err != nil {
+			return err
+		}
+		if _, err := w.Write(ansi); err != nil {
+			return err
+		}
+	}
+
+	if options.Background != "" {
+		ansi, ok := ansiBg[options.Background]
+		if !ok {
+			return fmt.Errorf("unsupported background color: %s", options.Background)
+		}
+		if _, err := w.Write([]byte{'\x1b'}); err != nil {
+			return err
+		}
+		if _, err := w.Write(ansi); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func resetStyle(w io.Writer) error {
+	if _, err := w.Write([]byte{'\x1b'}); err != nil {
+		return err
+	}
+	if _, err := w.Write([]byte("[0m")); err != nil {
+		return err
 	}
 
 	return nil
